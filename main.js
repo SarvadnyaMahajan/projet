f5fc4c66ba33a2d401e89a97032d4e43d3f8e615
Sarvadnya mahajan <80883695+SarvadnyaMahajan@users.noreply.github.com>
2021-05-10 22:51:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var first_name = "Ranbir Kapoor";
+console.log(first_name);
+
+var last_name = "Kapoor";
+console.log(last_name);
+
+var combine = first_name.concat (last_name);
+console.log(first_name+" "+last_name);
+
+combine.push("likes");
+console.log(combine);
+
+combine.push("cherry");
+console.log(combine);
